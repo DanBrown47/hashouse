0958fba67e2f3eecd88df394a4ece2c51a17a58b
DanBrown47 <danwand47@gmail.com>
2021-01-25 10:09:36 +0530

    stdhash -> hash :+1 | POST added

--- app.py
+++ app.py
@@ -9,20 +9,20 @@ app.config["DEBUG"] = True
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET' , 'POST'])
 def home():
     # We will come back here later 
     return """
     <html> HEllo W0rlD <hello> 
     """
-@app.route('/help', methods=['GET'])
+@app.route('/help', methods=['GET' , 'POST'])
     # We will come back here later 
 def help():
     return jsonify({'All commands':'commands'})
 
 
 
-@app.route('/stdhash/md5/', methods=['GET'])
+@app.route('/hash/md5/', methods=['GET' , 'POST'])
 def api_md5():
     string_in = request.args.get("query")
     res = hashlib.md5(string_in.encode()).hexdigest()
@@ -31,63 +31,63 @@ def api_md5():
 
 
 # SHA Family 
-@app.route('/stdhash/sha256/', methods=['GET'])
+@app.route('/hash/sha256/', methods=['GET' , 'POST'])
 def api_sha256():
     string_in = request.args.get("query")
     res = hashlib.md5(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha256'})
 
-@app.route('/stdhash/sha384/', methods=['GET'])
+@app.route('/hash/sha384/', methods=['GET' , 'POST'])
 def api_sha384():
     string_in = request.args.get("query")
     res = hashlib.sha384(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha384'})
 
-@app.route('/stdhash/sha224/', methods=['GET'])
+@app.route('/hash/sha224/', methods=['GET' , 'POST'])
 def api_sha224():
     string_in = request.args.get("query")
     res = hashlib.sha224(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha224'})
 
-@app.route('/stdhash/sha512/', methods=['GET'])
+@app.route('/hash/sha512/', methods=['GET' , 'POST'])
 def api_sha512():
     string_in = request.args.get("query")
     res = hashlib.sha512(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha512'})
 
-@app.route('/stdhash/sha1/', methods=['GET'])
+@app.route('/hash/sha1/', methods=['GET' , 'POST'])
 def api_sha1():
     string_in = request.args.get("query")
     res = hashlib.sha1(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha1'})
 
-@app.route('/stdhash/sha3_224/', methods=['GET'])
+@app.route('/hash/sha3_224/', methods=['GET' , 'POST'])
 def api_sha3_224():
     string_in = request.args.get("query")
     res = hashlib.sha3_224(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha3_224'})
 
-@app.route('/stdhash/sha3_256/', methods=['GET'])
+@app.route('/hash/sha3_256/', methods=['GET' , 'POST'])
 def api_sha3_256():
     string_in = request.args.get("query")
     res = hashlib.sha3_256(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha3_256'})
 
-@app.route('/stdhash/sha3_384/', methods=['GET'])
+@app.route('/hash/sha3_384/', methods=['GET' , 'POST'])
 def api_sha3_384():
     string_in = request.args.get("query")
     res = hashlib.sha3_384(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'sha3_384'})
 
-@app.route('/stdhash/sha3_512/', methods=['GET'])
+@app.route('/hash/sha3_512/', methods=['GET' , 'POST'])
 def api_sha3_512():
     string_in = request.args.get("query")
     res = hashlib.sha3_512(string_in.encode()).hexdigest()
@@ -95,7 +95,7 @@ def api_sha3_512():
                     'algo':'sha3_512'})
 
 #SHAKE FAMILY
-@app.route('/stdhash/shake_256/', methods=['GET'])
+@app.route('/hash/shake_256/', methods=['GET' , 'POST'])
 def api_shake_256():
     string_in = request.args.get("query")
     size = int(request.args.get("size"))
@@ -104,7 +104,7 @@ def api_shake_256():
                     'algo':'shake_265',
                     'size':size})
 
-@app.route('/stdhash/shake_128/', methods=['GET'])
+@app.route('/hash/shake_128/', methods=['GET' , 'POST'])
 def api_shake_128():
     string_in = request.args.get("query")
     size = int(request.args.get("size"))
@@ -115,14 +115,14 @@ def api_shake_128():
 
 
 #BLAKE FAMILY
-@app.route('/stdhash/blake2s/', methods=['GET'])
+@app.route('/hash/blake2s/', methods=['GET' , 'POST'])
 def api_blake2s():
     string_in = request.args.get("query")
     res = hashlib.blake2s(string_in.encode()).hexdigest()
     return jsonify({'result': res,
                     'algo':'blake2s'})
 
-@app.route('/stdhash/blake2b/', methods=['GET'])
+@app.route('/hash/blake2b/', methods=['GET' , 'POST'])
 def api_blake2b():
     string_in = request.args.get("query")
     res = hashlib.blake2b(string_in.encode()).digest()
